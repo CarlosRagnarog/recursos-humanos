a69812f6bc32289a84c8b3f1b5ba8d412cb13bb7
CarlosRagnarog <arevalomanuel233@gmail.com>
2026-06-04 10:12:02 -0400

    Configurar backend con Swagger y conexion PostgreSQL

--- Program.cs
+++ Program.cs
@@ -3,15 +3,22 @@ using RecursosHumanos.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
 
 if (app.Environment.IsDevelopment())
 {
